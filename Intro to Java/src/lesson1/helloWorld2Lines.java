d6546ea88b2e8fb8b8005634553535323ae2c7a6
chriscrutt <chriscrutt@gmail.com>
2017-07-10 02:05:10 -0700

    asdf
    Asdf

---
+++ Intro to Java/src/lesson1/helloWorld2Lines.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class helloWorld2Lines {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("World");
+		
+	}
+
+}
---
+++ Intro to Java/src/lesson1/printAndPrintln.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class printAndPrintln {
+
+	public static void main(String[] args) {
+		
+		System.out.print(3);
+		System.out.println(4+5);
+
+	}
+
+}
